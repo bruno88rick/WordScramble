03a35d780d7b4354988bd87b1ed13c4218e42769
Bruno Ricardo de Oliveira <bruno88.rick@hotmail.com>
2024-01-22 17:16:39 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Bruno Oliveira on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
